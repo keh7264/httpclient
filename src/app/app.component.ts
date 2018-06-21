bf2a93841b65c0b5e7d610444c670c3a18b8d358
ehkim <keh7264@gmail.com>
2018-06-21 22:48:33 +0900

    httpclient get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  id: number;
+  content: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  todos: Todo[];
+  url = 'http://localhost:3000/todos';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<Todo[]>(this.url)
+      .subscribe(todos => this.todos = todos);
+
+  }
 }
